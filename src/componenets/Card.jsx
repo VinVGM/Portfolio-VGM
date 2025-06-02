7652e196a1f110d6aee5b9c8ec7b2c0b63c6ff7c
Vineeth V <teamminers999@gmail.com>
2025-06-02 21:11:41 +0530

    Fixed a bug regarding cards

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion, scale } from 'motion/react'
 const Card = ({text,style, image, containerRef}) => {
-
+  const [ dragging, setIsDragging] = useState(false)
   return image && !text ? (
-    <motion.img className='absolute w-15 cursor-grab' 
+    <motion.img className='absolute w-16 cursor-grab' 
     src={image} 
-    style={style} 
-    whileHover={{scale:1.05}}
+    style={style}
+    animate = {{y:[0,-10,0]}}
+    transition={{
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeInOut"
+    }} 
+    whileHover={dragging ?  {scale:1.05} : {}}
     drag
+    onDragStart={() => setIsDragging(true)}
+    onDragEnd={()=>setIsDragging(false)}
     dragConstraints = {containerRef}
     dragElastic={1}
     />
   ) : (
-    <motion.div className='absolute px-1 ppy-4 text-xl text-center rounded-full ring ring-gray-400 font-extralight bg-storm w-[12rem] cursor-grab'
+    <motion.div className='absolute px-1 py-4 text-xl text-center rounded-full ring ring-gray-400 font-extralight bg-storm w-[12rem] cursor-grab'
         style = {style}
         whileHover={{scale:1.05}}
+        animate = {{y:[0,-10,0]}}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut"
+        }} 
         drag
         dragConstraints = {containerRef}
         dragElastic={1}
